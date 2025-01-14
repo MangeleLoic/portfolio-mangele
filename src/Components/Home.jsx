1ae66073eb318f30bfcb89b6f7d8c920c7b2477b
MangeleLoic <behonloic@gmail.com>
2025-01-14 14:00:52 +0100

    fixes

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -5,9 +5,9 @@ function Home() {
     <section id="home" className="text-center py-5">
       <div className="overlay"></div> 
       <div className="container">
-        <h1>Benvenuto!</h1>
+        <h1>Benvenuto nel mio portfolio!</h1>
         <p className="lead">Io sono Loïc Hernandez, sviluppatore full-stack.</p>
-        <a href="#projects" className="btn btn-primary">Scopri i miei progetti</a>
+        <a href="#projects" className="btn btn-primary">I miei progetti</a>
       </div>
     </section>
   );
